b5e448251f51615739dd2c9656e663e3fa46944a
karunkmr <karun.kmr03@gmail.com>
2023-01-20 14:16:45 +0530

    samples

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { React } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import PDF from './samples/pdf.pdf';
+import DOC from './samples/word.doc';
+import DOCX from './samples/word.docx';
+import PPT from './samples/powerpoint.ppt';
+import PPTX from './samples/powerpoint.pptx';
+import XLS from './samples/excel.xls';
+import XLSX from './samples/excel.xlsx';
 import ReCAPTCHA from "react-google-recaptcha";
 
 
@@ -10,7 +17,6 @@ function App() {
   function onChange(value) {
     console.log("Captcha value:", value);
   }
-
 
   return (
     <div className="App">
@@ -27,13 +33,14 @@ function App() {
         >
           Learn React
         </a>
-
         <ReCAPTCHA
           sitekey="6LecUsgjAAAAAI7fpja2OcU5qM9OuQFfxVzw7Dpd"
           onChange={onChange}
         />
 
       </header>
+      <iframe src={'https://view.officeapps.live.com/op/embed.aspx?src=' + DOC} width={'100%'} height={'400px'}></iframe>
+      <iframe src={'https://view.officeapps.live.com/op/embed.aspx?src=' + DOCX} width={'100%'} height={'400px'}></iframe>
     </div>
   );
 }
